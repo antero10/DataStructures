64c301297a70e171cda4da5b53e1479707d06f5e
Gustavo Sanchez <antero105@gmail.com>
2021-01-03 11:50:02 -0500

    Check brackets passed

--- Exam 1/1_brackets_in_code/check_brackets.py
+++ Exam 1/1_brackets_in_code/check_brackets.py
@@ -13,22 +13,25 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(next)
+            opening_brackets_stack.append(Bracket(next, i + 1))
             pass
         if next in ")]}":
             if len(opening_brackets_stack) == 0:
-                return 0
+                return i + 1
             top = opening_brackets_stack.pop()
-            if not are_matching(top, next):
-                return text.index(next) + 1
+            if not are_matching(top.char, next):
+                return i + 1
             pass
-    return len(opening_brackets_stack)
+
+    if len(opening_brackets_stack) > 0:
+        return opening_brackets_stack[0].position
+    return 0
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    if mismatch is None or mismatch == 0:
+    if mismatch == 0:
         print('Success')
     else:
         print(mismatch)
